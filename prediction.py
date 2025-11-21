ca6f5c7480b1c0dbcaf4088c94d2a9c51c650d56
T-D-Projects <tdott.math@t-online.de>
2025-11-21 21:02:43 +0100

    Update file paths for robust saving and loading

--- prediction.py
+++ prediction.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import joblib
 
@@ -5,14 +6,17 @@ import joblib
 # STEP 1: Load credit applications dataset.
 # --------------------------------------------------
 print("[INFO]     Loading dataset 'credit_applications.csv'...")
-df = pd.read_csv("credit_applications.csv")
+folder_path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(folder_path, "credit_applications.csv")
+df = pd.read_csv(path)
 print(f"[SUCCESS]  Dataset loaded: {len(df)} rows, {len(df.columns)} columns.\n")
 
 # --------------------------------------------------
 # STEP 2: Load fitted pipeline (preprocessing and logistic regression model).
 # --------------------------------------------------
 print("[INFO]     Loading pipeline 'pipeline.joblib' (preprocessing and logistic regression model)...")
-pipeline = joblib.load("pipeline.joblib")
+path = os.path.join(folder_path, "pipeline.joblib")
+pipeline = joblib.load(path)
 print("[SUCCESS]  Pipeline loaded.\n")
 
 # --------------------------------------------------
@@ -27,5 +31,6 @@ print("[SUCCESS]  Predictions generated.\n")
 # STEP 4: Save original dataset and predictions to a single CSV.
 # --------------------------------------------------
 print("[INFO]     Saving predictions...")
-df.to_csv("predictions.csv", index=False)
+path = os.path.join(folder_path, "predictions.csv")
+df.to_csv(path, index=False)
 print("[SUCCESS]  Credit applications and predictions saved as 'predictions.csv'.\n")
